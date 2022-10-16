7de9e123d55175024119de2e3d84dd11b66e9d34
David Montoya <aaronmontoya360@gmail.com>
2022-10-15 23:44:30 -0500

    Add: set controllers to create, show, delete and update a building

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -5,6 +5,48 @@ const getBuildings = async (req, res) => {
   res.json(response);
 };
 
+const createBuildings = async (req, res) => {
+  const { name, year, author, location, description } = req.body;
+  const response = await knex("buildings").insert({
+    name: name,
+    year: year,
+    author: author,
+    location: location,
+    description: description,
+  });
+  res.send("Building created successfully");
+};
+
+const showBuilding = async (req, res) => {
+  const id = req.params.id;
+  const response = await knex("buildings").where("id", id);
+  res.json(response);
+};
+
+const deleteBuilding = async (req, res) => {
+  const id = req.params.id;
+  const response = await knex("buildings").where("id", id).del();
+  console.log(response);
+  res.json(`building ${id} deleted successfully`);
+};
+
+const updateBuilding = async (req, res) => {
+  const id = req.params.id;
+  const { name, year, author, location, description } = req.body;
+  const response = await knex("buildings").where("id", id).update({
+    name: name,
+    year: year,
+    author: author,
+    location: location,
+    description: description,
+  });
+  res.json(`building ${id} updated successfully`);
+};
+
 module.exports = {
   getBuildings,
+  createBuildings,
+  showBuilding,
+  deleteBuilding,
+  updateBuilding,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getBuildings } = require("../controllers");
+const {
+  getBuildings,
+  createBuildings,
+  showBuilding,
+  deleteBuilding,
+  updateBuilding,
+} = require("../controllers");
 
 router.get("/buildings", getBuildings);
+router.post("/buildings", createBuildings);
+router.get("/buildings/:id", showBuilding);
+router.delete("/buildings/:id", deleteBuilding);
+router.patch("/buildings/:id", updateBuilding);
 
 module.exports = router;
