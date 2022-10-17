8a52a10c8525b2e4948b1e46df5b209831012091
David Montoya <aaronmontoya360@gmail.com>
2022-10-16 19:03:19 -0500

    Add:  image file and modify seed data

--- db/index.js
+++ db/index.js
@@ -13,7 +13,7 @@ const db = new sqlite3.Database(
 );
 
 db.run(
-  "CREATE TABLE IF NOT EXISTS buildings(id INTEGER PRIMARY KEY, name, year INTEGER, author, location, description)",
+  "CREATE TABLE IF NOT EXISTS buildings(id INTEGER PRIMARY KEY, name, year INTEGER, author, location, description, image)",
   (error) => {
     if (error) return console.error(error);
 
@@ -25,15 +25,19 @@ const data = fs.readFileSync("./db/seed.json");
 const parsedData = JSON.parse(data);
 
 const inputValues =
-  "INSERT INTO buildings (name, year, author, location, description) VALUES(?, ?, ?, ?, ?)";
+  "INSERT INTO buildings (name, year, author, location, description, image) VALUES(?, ?, ?, ?, ?, ?)";
 
 parsedData.forEach((element) => {
-  const { name, year, author, location, description } = element;
-  db.run(inputValues, [name, year, author, location, description], (error) => {
-    if (error) return console.error(error);
+  const { name, year, author, location, description, image } = element;
+  db.run(
+    inputValues,
+    [name, year, author, location, description, image],
+    (error) => {
+      if (error) return console.error(error);
 
-    console.log("New row created succesfully!!...");
-  });
+      console.log("New row created succesfully!!...");
+    }
+  );
 });
 
 db.close((error) => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 //midlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // routes
 app.use(require("./routes/index"));
